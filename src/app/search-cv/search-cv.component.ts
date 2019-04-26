6b261ebc5cc5b8a378d2c302ca29adbf41651276
vleletc <rozma.cv@gmail.com>
2019-04-26 17:07:25 +0300

    initial commit

--- src/app/search-cv/search-cv.component.ts
+++ src/app/search-cv/search-cv.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 
-import { SearchCVService } from "../services/search-cv.service";
-import { SearchCV } from "../models/searchCV.model";
-import { Person } from "../models/person.model";
+import { SearchCVService } from '../services/search-cv.service';
+import { SearchCV } from '../models/searchCV.model';
+import { Person } from '../models/person.model';
 
 @Component({
   selector: 'app-search-cv',
   templateUrl: './search-cv.component.html',
-  styleUrls: ['./search-cv.component.css']
+  styleUrls: ['./search-cv.component.scss']
 })
 
 export class SearchCVComponent {
