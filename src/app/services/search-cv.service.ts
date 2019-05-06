a27c4a065e6773b92616f18dfec6ad075e81d2d7
vleletc <rozma.cv@gmail.com>
2019-05-06 13:04:18 +0300

    for test

--- src/app/services/search-cv.service.ts
+++ src/app/services/search-cv.service.ts
@@ -6,7 +6,6 @@ const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
-    'Authorization': 'Basic dXNlcjp1c2Vy'
   })
 };
 
