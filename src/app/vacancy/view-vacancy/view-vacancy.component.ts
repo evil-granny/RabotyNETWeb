4e02a02b5af6776936b12b13632b8a264f26fac9
Den4ik18 <den.ohorodnik@gmail.com>
2019-05-15 19:41:51 +0300

    Deleted some method

--- src/app/vacancy/view-vacancy/view-vacancy.component.ts
+++ src/app/vacancy/view-vacancy/view-vacancy.component.ts
@@ -32,42 +32,16 @@ export class ViewVacancyComponent implements OnInit {
         });
     }
   }
-
-  // create(): void {
-  //   this.vacancyService.createVacancy(this.vacancy,this.route.snapshot.paramMap.get('companyName'))
-  //     .subscribe(data => {
-  //       this.router.navigate(['/viewCompany/'+ this.route.snapshot.paramMap.get('companyName')]);
-  //     }, error => console.error(error));
-  // };
-
-  // update(): void {
-  //   console.log(this.vacancy);
-  //     this.vacancyService.update(this.vacancy)
-  //      .subscribe(data => {
-  //        this.gotoList();
-  //      }, error => console.error(error));
-
-  // };
-
-
-  // gotoList() {
-  //   this.router.navigate(['/vacancies']);
-  // }
-
-  // employment = 'FULL PART_TIME HOURLY TRAINEE'.split(' ');
-  // selectedEmployment = 'FULL';
-
-  // changingValue(newValue) {
-  //   this.selectedEmployment = newValue;
-  // }
-
-
   get isCowner() {
     return this.currentUser && this.currentUser.roles  &&  this.currentUser.roles.indexOf(Role.ROLE_COWNER) > -1;
   }
 
   get isAdmin() {
     return this.currentUser && this.currentUser.roles  &&  this.currentUser.roles.indexOf(Role.ROLE_ADMIN) > -1;
+  }
+
+  get isUser() {
+    return this.currentUser && this.currentUser.roles  &&  this.currentUser.roles.indexOf(Role.ROLE_USER) > -1;
   }
 
 }
