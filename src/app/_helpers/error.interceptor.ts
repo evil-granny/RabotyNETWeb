4e5272e0c1b563e297e7862fff7214d3bdf978b9
vdudatc <L-Y'H0nO>
2019-05-16 15:49:20 +0300

    login ang registration changes

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -4,11 +4,12 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import {AuthenticationService} from '../services/authentication.service';
+import {Router} from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(private router: Router, private authenticationService: AuthenticationService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -16,12 +17,17 @@ export class ErrorInterceptor implements HttpInterceptor {
       console.log('---------SOME BACK ERROR--------')
       console.log(err)
       console.log(err.status)
-      if (err.status === 401 || err.status === 403) {
+      if (err.status === 401) {
         console.log('========== ERRORS ==========')
         console.log(err.status);
         // auto logout if 401 response returned from api
-        this.authenticationService.logout();
-        location.reload(true);
+        // this.authenticationService.logout();
+        // location.reload(true);
+      }
+      if (err.status === 403) {
+        console.log('========== ERRORS ==========')
+        console.log(err.status);
+        this.router.navigate(['/accessDenied']);
       }
 
       const error = err.error.message || err.statusText;
