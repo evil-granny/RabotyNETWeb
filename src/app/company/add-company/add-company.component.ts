016b2948bbec31d832a632707847ed85e0f0bbf8
Михайло Малярчук <michaelbodybuilder1999@gmail.com>
2019-05-21 18:59:35 +0300

    refactored companies

--- src/app/company/add-company/add-company.component.ts
+++ src/app/company/add-company/add-company.component.ts
@@ -55,7 +55,7 @@ export class AddCompanyComponent implements OnInit {
     this.companyService.update(this.company)
       .subscribe(data => {
         if (data != null)
-        this.router.navigate(['companies/my']);
+        this.router.navigate(['vacancies']);
         else
           alert("Validation problem has been occured");
       });
