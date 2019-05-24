55f89feae079dc991534d39df2078eddfb49f0f6
vdudatc <L-Y'H0nO>
2019-05-24 11:22:09 +0300

    configured global CORS

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,9 +10,6 @@ const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://localhost:4200',
-    'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE',
-    'Access-Control-Max-Age': '3600',
-    'Access-Control-Allow-Headers': 'X-requested-with, Content-Type',
   })
 };
 
