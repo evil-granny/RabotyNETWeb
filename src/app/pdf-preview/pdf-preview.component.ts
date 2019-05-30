465a0b4e4e46f22d8f4a5d2eb8c554ae4d219dae
Den4ik18 <den.ohorodnik@gmail.com>
2019-05-30 19:13:27 +0300

    Fixed update vacancy which has resume

--- src/app/pdf-preview/pdf-preview.component.ts
+++ src/app/pdf-preview/pdf-preview.component.ts
@@ -34,33 +34,19 @@ export class PdfPreviewComponent implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute, private pdfService: PdfService, public sanitizer: DomSanitizer,public dialog: MatDialog, private vacancyService: VacancyService, private resumeService : ResumeService) { }
 
   ngOnInit():void {
-
     var resumeId = this.route.snapshot.paramMap.get('resumeId');
-
     this.sendVacancy = this.route.snapshot.paramMap.get('vacancyId');
-
     this.pdfService.show(resumeId, this.send)
-
     .subscribe(data => {
-
       var file = new Blob([data], { type: 'application/pdf' });
-
       this.urlTemp = URL.createObjectURL(file);
-      
       this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.urlTemp);
-
     }); 
-
-
-
   }
 
   newWindowPdf():void {
-
     window.open(this.urlTemp);
-
     window.focus(); 
-
   }
 
 sendEmail(){
@@ -68,35 +54,26 @@ sendEmail(){
   else this.sendResumeForVacancy()
 }
 
-
   sendResumeForUser() {
     this.pdfService.send().subscribe();
     this.openModal("Mail send");
         
 }
 
-
-
   openModal(name: String) {
     this.dialog.open(ComfirmComponent, { data: { name } })
   }
 
   sendResumeForVacancy() {
-
     var vacancyId = this.route.snapshot.paramMap.get('vacancyId');
-    
     this.pdfService.findByUserId().subscribe(data=>{
     this.resume = data;
     this.vacancyService.sendResume(this.resume, vacancyId).subscribe(data => {
       this.openModal("Mail send");
     }, error => console.error(error));
   });
-  
-  	
-  
   }
-
-
+  
 }
 
 
