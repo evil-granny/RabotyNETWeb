99050a5f6ff91e7a4a370e51b45d56eec42a33fb
evil-granny <den.ohorodnik@gmail.com>
2020-04-18 10:20:42 +0300

    Added files for heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/angular-pro-sidebar'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname,'/dist/angular-pro-sidebar/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
