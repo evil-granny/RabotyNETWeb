0bb13faaa315fe093b194ff1dc51a44a98fc2049
evil-granny <den.ohorodnik@gmail.com>
2020-04-20 21:37:35 +0300

    Renamed application

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-pro-sidebar'));
+app.use(express.static('./dist/RabotyNET'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/angular-pro-sidebar/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/RabotyNET/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
